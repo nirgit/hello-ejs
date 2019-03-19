0407220fd2a67bbd0e2c18e63e4a7eac5db60ee8
Nir Moav <nirm@wix.com>
2019-03-19 11:27:05 +0200

    simple example for ejs with params

--- src/main.js
+++ src/main.js
@@ -6,7 +6,10 @@ app.set('view engine', 'ejs')
 app.set('views', path.join(__dirname, './views'));
 
 app.get('/', function(req, res) {
-    res.render('pages/index')
+    res.render('pages/index', {
+        name: 'Nir',
+        time: Date()
+    })
 })
 
 app.listen(8080)
